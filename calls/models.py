45a52afe85535fadbd7a9d2761ff590e7f06b59e
Fernando Júnior <junio.webmaster@gmail.com>
2019-07-15 00:57:32 -0300

    Add more tests and update some models to override save method

--- calls/models.py
+++ calls/models.py
@@ -1,10 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Call(models.Model):
 
     def __str__(self):
-        return 'call_id: {}, started at {} and ended at {}'.format(self.id, self.start.timestamp, self.end.timestamp)
+        return 'call_id: {}'.format(self.id)
 
 
 class CallStart(models.Model):
@@ -16,6 +17,16 @@ class CallStart(models.Model):
     def __str__(self):
         return 'call_id: {}, started at {}'.format(self.call.id, self.timestamp)
 
+    def save(self, *args, **kwargs):
+        if self.id:
+            try:
+                if self.timestamp > self.call.end.timestamp:
+                    raise ValueError("Start call timestamp can't be greater than the end call timestamp")
+            except CallEnd.DoesNotExist:
+                return super().save(*args, **kwargs)
+
+        super().save(*args, **kwargs)
+
 
 class CallEnd(models.Model):
     call = models.OneToOneField(Call, related_name='end', on_delete=models.CASCADE)
@@ -23,3 +34,11 @@ class CallEnd(models.Model):
 
     def __str__(self):
         return 'call_id: {}, ended at {}'.format(self.call.id, self.timestamp)
+
+    def save(self, *args, **kwargs):
+        if not CallStart.objects.filter(call=self.call).exists():
+            raise ValidationError('Not allowed to create a end call without a start call')
+        if self.timestamp < self.call.start.timestamp:
+            raise ValueError("End call timestamp can't be less than the start call timestamp")
+
+        super().save(*args, **kwargs)
