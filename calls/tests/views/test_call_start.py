4043b61c66525d58bc2ac6dae9f24133a7028538
Fernando Júnior <junio.webmaster@gmail.com>
2019-07-16 22:22:00 -0300

    Add test to get valid single call start

--- calls/tests/views/test_call_start.py
+++ calls/tests/views/test_call_start.py
@@ -10,10 +10,10 @@ class CallStartTestCase(APITestCase):
     def setUp(self):
         first_call = Call.objects.create(id=70)
         second_call = Call.objects.create(id=71)
-        CallStart.objects.create(call=first_call, timestamp='2016-02-29T12:00:00Z', source='99988526423',
-                                 destination='9933468278')
-        CallStart.objects.create(call=second_call, timestamp='2017-12-11T15:07:13Z', source='99988526423',
-                                 destination='9933468278')
+        self.first_call_start = CallStart.objects.create(call=first_call, timestamp='2016-02-29T12:00:00Z',
+                                                         source='99988526423', destination='9933468278')
+        self.second_call_start = CallStart.objects.create(call=second_call, timestamp='2017-12-11T15:07:13Z',
+                                                          source='99988526423', destination='9933468278')
 
     def test_should_get_all_started_calls_when_requested_by_the_client(self):
         response = self.client.get(reverse('calls:start-list'))
@@ -21,4 +21,12 @@ class CallStartTestCase(APITestCase):
         serializer = CallStartSerializer(started_calls, many=True)
 
         self.assertEqual(response.data['results'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_should_get_valid_single_call_start_when_requested_by_the_client(self):
+        response = self.client.get(reverse('calls:start-detail', kwargs={'pk': self.first_call_start.pk}))
+        call_start = CallStart.objects.get(pk=self.first_call_start.pk)
+        serializer = CallStartSerializer(call_start)
+
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
