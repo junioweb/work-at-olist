0251e772eb909fc34852168e08559fdbf56ca28f
Fernando Júnior <junio.webmaster@gmail.com>
2019-07-17 02:25:03 -0300

    Add view test to post call start and update serializer

--- calls/serializers.py
+++ calls/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 
-from .models import CallStart
+from .models import Call, CallStart
 
 
 class CallStartSerializer(serializers.ModelSerializer):
-    call_id = serializers.ReadOnlyField(source='call.id')
+    call_id = serializers.PrimaryKeyRelatedField(source='call', queryset=Call.objects.all())
 
     class Meta:
         model = CallStart
