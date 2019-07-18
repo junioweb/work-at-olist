783c2ab45e8de07351c298dd71d659423aba20ee
Fernando Júnior <junio.webmaster@gmail.com>
2019-07-18 01:24:02 -0300

    Add view test to get single call

--- calls/tests/views/call/test_get.py
+++ calls/tests/views/call/test_get.py
@@ -8,14 +8,14 @@ from calls.serializers import CallSerializer
 
 class GetTestCase(APITestCase):
     def setUp(self):
-        first_call = Call.objects.create(id=70)
-        second_call = Call.objects.create(id=71)
-        self.first_call_start = CallStart.objects.create(call=first_call, timestamp='2016-02-29T12:00:00Z',
-                                                         source='99988526423', destination='9933468278')
-        self.second_call_start = CallStart.objects.create(call=second_call, timestamp='2017-12-11T15:07:13Z',
-                                                          source='99988526423', destination='9933468278')
-        self.first_call_end = CallEnd.objects.create(call=first_call, timestamp='2016-02-29T14:00:00Z')
-        self.second_call_end = CallEnd.objects.create(call=second_call, timestamp='2017-12-11T15:14:56Z')
+        self.first_call = Call.objects.create(id=70)
+        self.second_call = Call.objects.create(id=71)
+        CallStart.objects.create(call=self.first_call, timestamp='2016-02-29T12:00:00Z',
+                                 source='99988526423', destination='9933468278')
+        CallStart.objects.create(call=self.second_call, timestamp='2017-12-11T15:07:13Z',
+                                 source='99988526423', destination='9933468278')
+        CallEnd.objects.create(call=self.first_call, timestamp='2016-02-29T14:00:00Z')
+        CallEnd.objects.create(call=self.second_call, timestamp='2017-12-11T15:14:56Z')
 
     def test_should_get_all_calls_when_requested_by_the_client(self):
         response = self.client.get(
@@ -24,4 +24,19 @@ class GetTestCase(APITestCase):
         serializer = CallSerializer(calls, many=True)
 
         self.assertEqual(response.data['results'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_should_get_valid_single_call_when_requested_by_the_client(self):
+        response = self.client.get(
+            reverse('calls:-detail', kwargs={'pk': self.first_call.pk}))
+        call = Call.objects.get(pk=self.first_call.pk)
+        serializer = CallSerializer(call)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_should_return_status_code_200_and_empty_data_when_get_invalid_pk(self):
+        response = self.client.get(
+            reverse('calls:-detail', kwargs={'pk': 27}))
+        self.assertEqual(response.data, {})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
