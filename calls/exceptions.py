45a2be3f378c869abfeb8c99899404c69b1bedd1
Fernando Júnior <junio.webmaster@gmail.com>
2019-08-02 13:44:48 -0300

    Update custom exceptions and change how handle exception in views

--- calls/exceptions.py
+++ calls/exceptions.py
@@ -1,43 +1,37 @@
-from django.core.exceptions import ValidationError
+from rest_framework.exceptions import APIException
 
 
-class CallStartMissingError(ValidationError):
-    def __init__(self, message=None, code=None, params=None):
-        if not message:
-            message = 'Not allowed to create a end call without a start call'
-        super().__init__(message, code, params)
+class CallStartMissingError(APIException):
+    status_code = 422
+    default_detail = 'Not allowed to create a end call without a start call'
+    default_code = 'call_start_missing_error'
 
 
-class TimestampLessThanCallStartTimestampError(ValidationError):
-    def __init__(self, message=None, code=None, params=None):
-        if not message:
-            message = 'End call timestamp can\'t be less than the start call timestamp'
-        super().__init__(message, code, params)
+class TimestampLessThanCallStartTimestampError(APIException):
+    status_code = 422
+    default_detail = 'End call timestamp can\'t be less than the start call timestamp'
+    default_code = 'timestamp_less_than_call_start_timestamp_error'
 
 
-class TimestampGreaterThanCallEndTimestampError(ValidationError):
-    def __init__(self, message=None, code=None, params=None):
-        if not message:
-            message = 'Start call timestamp can\'t be greater than the end call timestamp'
-        super().__init__(message, code, params)
+class TimestampGreaterThanCallEndTimestampError(APIException):
+    status_code = 422
+    default_detail = 'Start call timestamp can\'t be greater than the end call timestamp'
+    default_code = 'timestamp_greater_than_call_end_timestamp_error'
 
 
-class TypeCallMissingError(ValidationError):
-    def __init__(self, message=None, code=None, params=None):
-        if not message:
-            message = 'Not allowed to create or update a call record without a field type'
-        super().__init__(message, code, params)
+class TypeCallMissingError(APIException):
+    status_code = 400
+    default_detail = 'Not allowed to create or update a call record without a field type'
+    default_code = 'type_call_missing_error'
 
 
-class RecordsMissingError(ValidationError):
-    def __init__(self, message=None, code=None, params=None):
-        if not message:
-            message = 'Not allowed to create or update a call without a list field records'
-        super().__init__(message, code, params)
+class RecordsMissingError(APIException):
+    status_code = 400
+    default_detail = 'Not allowed to create or update a call without a list field records'
+    default_code = 'records_missing_error'
 
 
-class EmptyRecordsError(ValidationError):
-    def __init__(self, message=None, code=None, params=None):
-        if not message:
-            message = 'Not allowed to create or update a call with empty records'
-        super().__init__(message, code, params)
+class EmptyRecordsError(APIException):
+    status_code = 400
+    default_detail = 'Not allowed to create or update a call with empty records'
+    default_code = 'empty_records_error'
--- calls/views.py
+++ calls/views.py
@@ -5,8 +5,7 @@ from django.urls import reverse
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
-from .exceptions import CallStartMissingError, TimestampLessThanCallStartTimestampError
-from .exceptions import TimestampGreaterThanCallEndTimestampError, TypeCallMissingError
+from .exceptions import TypeCallMissingError
 from .exceptions import RecordsMissingError, EmptyRecordsError
 
 from .models import Call, CallEnd, CallStart
@@ -15,14 +14,6 @@ from .serializers import CallSerializer, CallEndSerializer, CallStartSerializer
 
 
 class BaseViewSet(viewsets.ModelViewSet):
-    def create(self, request, *args, **kwargs):
-        try:
-            response = super().create(request, *args, **kwargs)
-        except TypeCallMissingError as err:
-            response = Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-
-        return response
-
     def retrieve(self, request, *args, **kwargs):
         try:
             response = super().retrieve(request, *args, **kwargs)
@@ -44,28 +35,10 @@ class CallStartViewSet(BaseViewSet):
     queryset = CallStart.objects.all()
     serializer_class = CallStartSerializer
 
-    def update(self, request, *args, **kwargs):
-        try:
-            response = super().update(request, *args, **kwargs)
-        except TimestampGreaterThanCallEndTimestampError as err:
-            response = Response({'detail': err.message}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
-        return response
-
 
 class CallEndViewSet(BaseViewSet):
     queryset = CallEnd.objects.all()
     serializer_class = CallEndSerializer
-
-    def create(self, request, *args, **kwargs):
-        try:
-            response = super().create(request, *args, **kwargs)
-        except CallStartMissingError as err:
-            response = Response({'detail': err.message}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-        except TimestampLessThanCallStartTimestampError as err:
-            response = Response({'detail': err.message}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
-        return response
 
 
 class CallViewSet(BaseViewSet):
@@ -100,14 +73,6 @@ class CallViewSet(BaseViewSet):
                     end_serializer = CallEndSerializer(data=record)
                     end_serializer.is_valid(raise_exception=True)
                     end_serializer.save()
-        except RecordsMissingError as err:
-            return Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-        except EmptyRecordsError as err:
-            return Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-        except TypeCallMissingError as err:
-            return Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as err:
-            raise err
         finally:
             call.delete()
 
@@ -119,34 +84,25 @@ class CallViewSet(BaseViewSet):
         start_serializer = None
         end_serializer = None
 
-        try:
-            if 'records' not in request.data:
-                raise RecordsMissingError()
-            if not len(request.data['records']):
-                raise EmptyRecordsError()
+        if 'records' not in request.data:
+            raise RecordsMissingError()
+        if not len(request.data['records']):
+            raise EmptyRecordsError()
 
-            for record in request.data['records']:
-                type_call = record.get('type')
-                if type_call is None:
-                    raise TypeCallMissingError()
+        for record in request.data['records']:
+            type_call = record.get('type')
+            if type_call is None:
+                raise TypeCallMissingError()
 
-                record['call_id'] = call_instance.id
-                if type_call == 'start':
-                    start_instance = call_instance.start
-                    start_serializer = CallStartSerializer(start_instance, data=record, partial=partial)
-                    start_serializer.is_valid(raise_exception=True)
-                elif type_call == 'end':
-                    end_instance = call_instance.end
-                    end_serializer = CallEndSerializer(end_instance, data=record, partial=partial)
-                    end_serializer.is_valid(raise_exception=True)
-        except RecordsMissingError as err:
-            return Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-        except EmptyRecordsError as err:
-            return Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-        except TypeCallMissingError as err:
-            return Response({'detail': err.message}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as err:
-            raise err
+            record['call_id'] = call_instance.id
+            if type_call == 'start':
+                start_instance = call_instance.start
+                start_serializer = CallStartSerializer(start_instance, data=record, partial=partial)
+                start_serializer.is_valid(raise_exception=True)
+            elif type_call == 'end':
+                end_instance = call_instance.end
+                end_serializer = CallEndSerializer(end_instance, data=record, partial=partial)
+                end_serializer.is_valid(raise_exception=True)
 
         if start_serializer is not None:
             start_serializer.save()
