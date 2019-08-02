f7fddf3888b38be2c8e71ba7354fa895f1382e54
Fernando Júnior <junio.webmaster@gmail.com>
2019-08-02 19:42:07 -0300

    Update model test to calls

--- calls/tests/models/test_call.py
+++ calls/tests/models/test_call.py
@@ -1,7 +1,6 @@
-from django.db import IntegrityError
 from django.test import TestCase
 
-from calls.models import Call, CallStart, CallEnd
+from calls.models import Call
 
 
 class CallTestCase(TestCase):
@@ -11,12 +10,3 @@ class CallTestCase(TestCase):
         new_count = Call.objects.count()
 
         self.assertNotEqual(old_count, new_count)
-
-    def test_should_throw_exception_when_a_call_record_pair_already_exists(self):
-        call = Call.objects.create(id=66)
-        CallStart.objects.create(call=call, timestamp='2016-02-29T12:00:00Z', source='99988526423',
-                                 destination='9933468278')
-        CallEnd.objects.create(call=call, timestamp='2016-02-29T14:00:00Z')
-
-        with self.assertRaises(IntegrityError):
-            CallEnd.objects.create(call=call, timestamp='2016-02-29T15:00:00Z')
