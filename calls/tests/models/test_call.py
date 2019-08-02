3fc5602f582ac423b89ddc257d91144f3fcf1b0a
Fernando Júnior <junio.webmaster@gmail.com>
2019-08-02 19:34:48 -0300

    Update model test to calls

--- calls/tests/models/test_call.py
+++ calls/tests/models/test_call.py
@@ -5,21 +5,18 @@ from calls.models import Call, CallStart, CallEnd
 
 
 class CallTestCase(TestCase):
-    def setUp(self):
-        self.call = Call(id=69)
-
     def test_should_be_able_to_create_a_call_when_saving_the_object(self):
         old_count = Call.objects.count()
-        self.call.save()
+        Call.objects.create(id=69)
         new_count = Call.objects.count()
 
         self.assertNotEqual(old_count, new_count)
 
     def test_should_throw_exception_when_a_call_record_pair_already_exists(self):
-        self.call.save()
-        CallStart.objects.create(call=self.call, timestamp='2016-02-29T12:00:00Z', source='99988526423',
+        call = Call.objects.create(id=66)
+        CallStart.objects.create(call=call, timestamp='2016-02-29T12:00:00Z', source='99988526423',
                                  destination='9933468278')
-        CallEnd.objects.create(call=self.call, timestamp='2016-02-29T14:00:00Z')
+        CallEnd.objects.create(call=call, timestamp='2016-02-29T14:00:00Z')
 
         with self.assertRaises(IntegrityError):
-            CallEnd.objects.create(call=self.call, timestamp='2016-02-29T15:00:00Z')
+            CallEnd.objects.create(call=call, timestamp='2016-02-29T15:00:00Z')
