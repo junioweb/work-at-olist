1666fd21b4e2f11ed0727b4e084d33050e607712
Fernando Júnior <junio.webmaster@gmail.com>
2019-08-07 01:34:55 -0300

    Update method to check time range in price rules

--- bills/domain/price_rule.py
+++ bills/domain/price_rule.py
@@ -8,7 +8,6 @@ class PriceRule:
         self.end_time = kwargs.get('end_time', datetime.time(hour=23, minute=59))
         self.standing_charge = kwargs.get('standing_charge', 0.36)
         self.minute_charge = kwargs.get('minute_charge', 0.09)
-        self.has_plus_day = kwargs.get('has_plus_day', False)
 
     def calculate_price(self, duration, with_connection=True):
         price = duration * self.minute_charge
@@ -19,6 +18,6 @@ class PriceRule:
 
     def time_in_range(self, time):
         if self.start_time <= self.end_time:
-            return self.start_time <= time <= self.end_time
+            return self.start_time <= time < self.end_time
         else:
-            return self.start_time <= time or time <= self.end_time
+            return self.start_time <= time or time < self.end_time
