48adce8d8acec9b11403915c1940174b237194d4
Fernando Júnior <junio.webmaster@gmail.com>
2019-08-08 11:40:21 -0300

    Update bill serializers

--- bills/serializers.py
+++ bills/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 
-class CallRecordSerializer(serializers.Serializer):
+class BillCallRecordSerializer(serializers.Serializer):
     destination = serializers.CharField(min_length=10, max_length=11, read_only=True)
     start_date = serializers.DateField(read_only=True)
     start_time = serializers.TimeField(read_only=True)
@@ -16,7 +16,7 @@ class BillSerializer(serializers.Serializer):
     subscriber = serializers.CharField(min_length=10, max_length=11, read_only=True)
     period = serializers.CharField(min_length=6, max_length=7, read_only=True)
     total = serializers.SerializerMethodField()
-    call_records = CallRecordSerializer(many=True)
+    call_records = BillCallRecordSerializer(many=True, read_only=True)
 
     def get_total(self, obj):
         return 'R$ %.2f' % obj.total
